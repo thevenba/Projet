967c7fe50be47c2696e59518dc7cda05a74edabf
Anthony Gelibert <anthony.gelibert@me.com>
2015-04-09 16:46:29 +0200

    Simplify Color.

--- src/fr/iutvalence/java/tp/rushhour/Color.java
+++ src/fr/iutvalence/java/tp/rushhour/Color.java
@@ -1,78 +1,38 @@
 package fr.iutvalence.java.tp.rushhour;
 
 /**
- * Enum of vehicles colors
- * 
+ * Vehicle's colors.
+ *
  * @author thevenba
  * @version 1.0.0
  */
-public enum Color
-{
-	/**
-	 * Red color
-	 */
-	RED,
-	
-	/**
-	 * Light blue color
-	 */
-	LIGHT_BLUE,
-	
-	/**
-	 * Dark blue color
-	 */
-	DARK_BLUE,
-	
-	/**
-	 * Yellow color
-	 */
-	YELLOW,
-	
-	/**
-	 * orange color
-	 */
-	ORANGE,
-	
-	/**
-	 * Light green color
-	 */
-	LIGHT_GREEN,
-	
-	/**
-	 * Dark green color
-	 */
-	DARK_GREEN,
-	
-	/**
-	 * Purple color
-	 */
-	PURPLE;
-	
-	/**
-	 * @see java.lang.Enum#toString()
-	 */
-	public String toString()
-	{
-		switch (this)
-		{
-			case RED: 
-				return "R ";
-			case LIGHT_BLUE: 
-				return "lb";
-			case DARK_BLUE:
-				return "db";
-			case YELLOW:
-				return "y ";
-			case ORANGE:
-				return "o ";
-			case LIGHT_GREEN:
-				return "lg";
-			case DARK_GREEN:
-				return "dg";
-			case PURPLE:
-				return "p ";
-			default:
-				return null;
-		}
-	}
+public enum Color {
+    /** Red color. */
+    RED("R "),
+    /** Light blue color. */
+    LIGHT_BLUE("lb"),
+    /** Dark blue color. */
+    DARK_BLUE("db"),
+    /** Yellow color. */
+    YELLOW("y "),
+    /** Orange color. */
+    ORANGE("o "),
+    /** Light green color. */
+    LIGHT_GREEN("lg"),
+    /** Dark green color. */
+    DARK_GREEN("dg"),
+    /** Purple color. */
+    PURPLE("p ");
+    /* TODO JAVADOC. */
+    private final String display;
+
+    /* TODO JAVADOC. */
+    Color(final String disp) {
+        display = disp;
+    }
+
+    @Override
+    public String toString() {
+        return display;
+    }
 }
