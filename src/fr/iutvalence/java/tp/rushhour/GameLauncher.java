3d23f62bb3f1c52576b1ac38118f96fa95879930
Anthony Gelibert <anthony.gelibert@me.com>
2015-04-09 16:30:33 +0200

    GameLauncher.java: add a private constructor.
    GameLauncher is a typical utility class and consequently should not be
    instantiated.

--- src/fr/iutvalence/java/tp/rushhour/GameLauncher.java
+++ src/fr/iutvalence/java/tp/rushhour/GameLauncher.java
@@ -1,19 +1,21 @@
 package fr.iutvalence.java.tp.rushhour;
 
 /**
- * Launch a game
+ * Launch a game.
  *
  * @author duboitho
  * @version 1.1.0
  */
 public class GameLauncher {
+    /** Private and empty constructor. */
+    private GameLauncher() { /* NOTHING. */}
 
     /**
-     * Where the application starts
+     * Where the application starts.
      *
      * @param args not used
      */
-    public static void main(String[] args) {
+    public static void main(final String... args) {
         System.out.println(new Level1());
     }
 }
